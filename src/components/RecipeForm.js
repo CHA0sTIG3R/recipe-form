5fe3a6e0f09b9d005ef6550c8c8e11efc3c9a4a1
Hamzat Olowu <olowuhamzat@gmail.com>
2022-09-25 19:10:41 -0400

    Recipe form UI

---
+++ src/components/RecipeForm.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Field, Form, Formik, useField } from 'formik';
+
+const MyTextArea = ({ label, ...props }) => {
+    const [field, meta] = useField(props);
+    return (
+        <div>
+            <label htmlFor={props.id || props.name}>{label}</label>
+            <textarea {...field} {...props} />
+            {meta.touched && meta.error ? (
+                <div>{meta.error}</div>
+            ): null}
+        </div>
+    );
+};
+
+function RecipeForm() {
+
+    return (
+        <div>
+            <Formik
+                initialValues = {{
+                    name: '',
+                    type: '',
+                    ingredients: '',
+                    directions: '',
+                }}
+                onSubmit = {values => {
+                    values.ingredients = values.ingredients.split(",");
+                    values.directions = values.directions.split(",")
+                    console.log(JSON.stringify(values, null, 2));
+                }}>
+                <Form>
+                    <label>Recipe Name</label>
+                    <Field
+                        label='Recipe Name'
+                        name='name'
+                    />
+
+                    <label>Recipe Type</label>
+                    <Field label='Type' name='type' as="select" htmlFor='type'>
+                        <option value=''>Select Recipe Type</option>
+                        <option value='Pastry'>Pastry</option>
+                        <option value='Culinary'>Culinary</option>
+                    </Field>
+
+                    <MyTextArea 
+                        label='Ingredients'
+                        name='ingredients'
+                    />
+                    
+                    <MyTextArea 
+                        label='Directions'
+                        name='directions'
+                    />
+                    <button type='submit'>Submit</button>
+                </Form>
+            </Formik>
+        </div>
+    )
+}
+
+export default RecipeForm
